b55d4f791a2b2872fe146da403f52fb601c9beee
yamasy1549 <sanae@yamasy.info>
2017-04-28 09:53:57 +0900

    2週目「単語N-gram」

---
+++ mecab_ngram.rb
@@ -0,0 +1,65 @@
+require 'pry'
+require 'natto'
+
+class Array
+  def ngram(n)
+    self.each_cons(n).map(&:join)
+  end
+end
+
+class String
+  def wakati
+    nm = Natto::MeCab.new('-S%f[6] -F%f[6] -E\n')
+    nm.enum_parse(self).map do |n|
+      n.feature
+    end
+  end
+end
+
+def tf(frec, all)
+  Rational(frec, all)
+end
+
+def idf(n, df)
+  Math::log2(Rational(n, df)) + 1
+end
+
+def tf_idf(freq, all, n, df)
+  tf(freq, all) * idf(n, df)
+end
+
+word_table = {}
+source_files = {}
+
+Dir.open(ARGV[1]).each do |file_name|
+  next if file_name =~ /^\.+$/
+  File.open(File.join(ARGV[1], file_name)) do |file|
+    ngram = file.read.wakati.ngram(ARGV[0].to_i)
+    ngram.uniq.each do |name|
+      word_table[name] ||= {}
+      word_table[name][file_name] = ngram.grep(name).count
+    end
+    source_files[file_name] = ngram.count
+    puts "#{file_name} checked"
+  end
+end
+
+word_table = word_table.sort
+
+all_file_count = source_files.count
+
+source_files.each do |filename, word_count|
+  words_tf_idf = []
+  word_table.each do |name, word_count_in|
+    freq = word_count_in[filename]
+    df = word_count_in.count
+    if freq && word_count && all_file_count && df
+      words_tf_idf << "#{name}\t#{tf_idf(freq, word_count, all_file_count, df)}"
+    end
+  end
+  File.open(File.join(ARGV[2], "#{filename}-#{ARGV[0]}gram"), 'w') do |file|
+    words_tf_idf.each { |word_tf_idf| file.puts word_tf_idf }
+  end
+end
+
+# ruby mecab_ngram.rb 2 ./data ./result-word
