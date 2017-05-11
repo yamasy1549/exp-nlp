3df744fe46485e404ab4fd8aab9c0b4984ab93ef
yamasy1549 <sanae@yamasy.info>
2017-05-11 17:12:46 +0900

    文字N-gramでも記号を削除

--- mecab_ngram.rb
+++ mecab_ngram.rb
@@ -7,7 +7,7 @@ source_files = {}
 Dir.open(ARGV[1]).each do |file_name|
   next if file_name =~ /^\.+$/
   File.open(File.join(ARGV[1], file_name)) do |file|
-    ngram = file.read.chomp.remove_url.wakati(pos).ngram(ARGV[0].to_i)
+    ngram = file.read.chomp.remove_url.remove_symbols.wakati(pos).ngram(ARGV[0].to_i)
     word_table.collect_words(ngram, file_name)
     source_files[file_name] = ngram.count
     puts "#{file_name} checked"
@@ -24,7 +24,7 @@ source_files.each do |filename, word_count|
     df = word_count_in.count
     if freq && word_count && all_file_count && df
       tf_idf = tf_idf(freq, word_count, all_file_count, df)
-      words_tf_idf << "#{name}\t#{tf_idf}" if tf_idf > 0.02
+      words_tf_idf << "#{name}\t#{tf_idf}"
     end
   end
   File.open(File.join(ARGV[2], "#{filename}-#{ARGV[0]}gram"), 'w') do |file|
