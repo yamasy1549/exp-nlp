59f852691435e143b26163c40558e0d15dfa22f6
yamasy1549 <sanae@yamasy.info>
2017-05-11 12:07:20 +0900

    tf-idfが小さいものから 3-(N-1)*0.5 割を削除

--- ngram.rb
+++ ngram.rb
@@ -6,7 +6,7 @@ source_files = {}
 Dir.open(ARGV[1]).each do |file_name|
   next if file_name =~ /^\.+$/
   File.open(File.join(ARGV[1], file_name)) do |file|
-    ngram = file.read.ngram(ARGV[0].to_i)
+    ngram = file.read.remove_url.remove_symbols.ngram(ARGV[0].to_i)
     word_table.collect_words(ngram, file_name)
     source_files[file_name] = ngram.count
     puts "#{file_name} checked"
@@ -18,15 +18,25 @@ all_file_count = source_files.count
 
 source_files.each do |filename, word_count|
   words_tf_idf = []
+  tf_idf_list = []
   word_table.each do |name, word_count_in|
     freq = word_count_in[filename]
     df = word_count_in.count
     if freq && word_count && all_file_count && df
-      words_tf_idf << "#{name}\t#{tf_idf(freq, word_count, all_file_count, df)}"
+      tf_idf = tf_idf(freq, word_count, all_file_count, df)
+      tf_idf_list << tf_idf
+      words_tf_idf << "#{name}\t#{tf_idf}"
     end
   end
+
+  threshold = (tf_idf_list.max - tf_idf_list.min) * (1-(3-(ARGV[0].to_i-1)*0.5)) * 0.1
+
   File.open(File.join(ARGV[2], "#{filename}-#{ARGV[0]}gram"), 'w') do |file|
-    words_tf_idf.each { |word_tf_idf| file.puts word_tf_idf }
+    words_tf_idf.each do |word_tf_idf|
+      if word_tf_idf.match(/(?:.*)\t(.*)/)[1].to_f > threshold
+        file.puts word_tf_idf
+      end
+    end
   end
 end
 
